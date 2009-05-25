de46474be5c2a37f757366b2afc263a8d44c35a7
Max Cutler <max@maxcutler.com>
2009-05-25 04:01:40 +0000

    Fixed column introspection for Attribute model so that it happens once instead of for each Attribute model instance instantiation.

--- courant/core/dynamic_models/models.py
+++ courant/core/dynamic_models/models.py
@@ -120,12 +120,12 @@ class Attribute(models.Model):
         # Fields added through the admin at runtime will get dynamically added (see
         # DynamicTypeField's save() function), so this only needs to run once at server/
         # python initialization
-        if not hasattr(self, '_introspected_columns'):
+        if not hasattr(Attribute, '_introspected_columns'):
             columns = DynamicTypeField.objects.distinct('column').values_list('column', 'value_type')
             for column, value_type in columns:
                 field = DynamicTypeField.get_field_for_type(column, column, value_type)
                 Attribute.add_to_class(column, field)
-            self._introspected_columns=True
+            setattr(Attribute, '_introspected_columns', True)
         super(Attribute, self).__init__(*args, **kwargs)
 
 
