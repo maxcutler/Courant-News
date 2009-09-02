685aac0ba79d9f69e39312268eaffa42897da586
Max Cutler <max@maxcutler.com>
2009-09-02 04:41:21 +0000

    Added display and search fields to staffer admin page.

--- courant/core/staff/admin.py
+++ courant/core/staff/admin.py
@@ -5,6 +5,6 @@ from courant.core.staff.models import *
 
 class StafferAdmin(admin.ModelAdmin):
 
-    list_display = ['user', 'position']
-    search_fields = ['user__username', 'user__first_name', 'user__last_name']
+    list_display = ['last_name', 'first_name', 'position', 'user']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'first_name', 'last_name']
 admin.site.register(Staffer, StafferAdmin)
