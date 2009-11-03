63aca8759eca15ff2a8929818cfaa99e85cfe859
Max Cutler <max@maxcutler.com>
2009-11-03 23:42:24 +0000

    Added two more indices to countthis app to optimize common queries.

---
+++ courant/core/countthis/migrations/0003_more_indices.py
@@ -0,0 +1,37 @@
+
+from south.db import db
+from django.db import models
+from courant.core.countthis.models import *
+
+class Migration:
+
+    def forwards(self, orm):
+        db.create_index('countthis_statistic', ['content_type_id', 'object_id'])
+        db.create_index('countthis_statistic', ['path'])
+
+
+    def backwards(self, orm):
+        db.delete_index('countthis_statistic', ['content_type_id', 'object_id'])
+        db.delete_index('countthis_statistic', ['path'])
+
+
+    models = {
+        'countthis.statistic': {
+            'Meta': {'unique_together': "['content_type','object_id']"},
+            'comment_count': ('models.PositiveIntegerField', [], {'default': '0'}),
+            'content_type': ('models.ForeignKey', ["orm['contenttypes.ContentType']"], {}),
+            'created_at': ('CreationDateTimeField', [], {}),
+            'email_count': ('models.PositiveIntegerField', [], {'default': '0'}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('models.PositiveIntegerField', [], {}),
+            'path': ('models.CharField', [], {'max_length': '255'}),
+            'view_count': ('models.PositiveIntegerField', [], {'default': '0'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label','model'),)", 'db_table': "'django_content_type'"},
+            '_stub': True,
+            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['countthis']
