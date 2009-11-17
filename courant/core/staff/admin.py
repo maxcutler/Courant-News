52c40d8c1af4225dd22e4dc65c5e9131f122e04d
Robert Baskin <robert@rsbaskin.com>
2009-11-17 05:14:00 +0000

    Staff refactor. I do all the hard models work and now Max just has to write the easy migration script - typical.

--- courant/core/staff/admin.py
+++ courant/core/staff/admin.py
@@ -5,7 +5,11 @@ from courant.core.staff.models import *
 
 class StafferAdmin(admin.ModelAdmin):
 
-    list_display = ['last_name', 'first_name', 'position', 'user', 'public_profile']
+    list_display = ['last_name', 'first_name', 'user', 'public_profile']
     list_filter = ['public_profile']
     search_fields = ['user__username', 'user__first_name', 'user__last_name', 'first_name', 'last_name']
 admin.site.register(Staffer, StafferAdmin)
+
+admin.site.register(StaffPositionTitle)
+admin.site.register(StaffDepartment)
+admin.site.register(StaffPosition)
--- courant/core/staff/models.py
+++ courant/core/staff/models.py
@@ -3,59 +3,91 @@ from django.contrib.auth.models import User
 
 from courant.core.gettag import gettag
 
+from datetime import date
+
+class StaffPositionTitle(models.Model):
+
+	name = models.CharField(max_length=255)
+	
+	def __unicode__(self):
+		return self.name
+	
+class StaffDepartment(models.Model):
+
+	name = models.CharField(max_length=255)
+	
+	def __unicode__(self):
+		return self.name
+
+class StaffPositionManager(models.Manager):
+	use_for_related_fields = True
+
+	def active(self):
+		return self.get_query_set().filter(start_on__lte=date.today(), end_on__gte=date.today())
+
+class StaffPosition(models.Model):
+
+	name = models.CharField(max_length=255)
+	
+	title = models.ForeignKey(StaffPositionTitle)
+	position = models.ForeignKey(StaffDepartment)
+	
+	start_on = models.DateField()
+	end_on = models.DateField()
+	
+	objects = StaffPositionManager()
+	
+	def __unicode__(self):
+		return self.name
+
 class Staffer(models.Model):
-    """
-    Represents a staff member in the news organization, or anyone who publishes
-    content on the web site.
-    """
-    user = models.OneToOneField(User, blank=True, null=True,
-                                help_text="Optional, but recommended.")
-    slug = models.SlugField()
-    position = models.CharField(max_length=255)
+	"""
+	Represents a staff member in the news organization, or anyone who publishes
+	content on the web site.
+	"""
+	slug = models.SlugField()
+	
+	user = models.OneToOneField(User, blank=True, null=True,
+								help_text="Optional, but recommended.")
+	positions = models.ManyToManyField(StaffPosition)
 
-    # denormalization if a user is linked to this staffer, or else holds the
-    # staffer's name if they do not have a user account
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
-    
-    public_profile = models.BooleanField(default=True)
+	# denormalization if a user is linked to this staffer, or else holds the
+	# staffer's name if they do not have a user account
+	first_name = models.CharField(max_length=30, blank=True)
+	last_name = models.CharField(max_length=30, blank=True)
+	
+	bio = models.TextField()
+	public_profile = models.BooleanField(default=True)
 
-    class Meta:
-        ordering = ['user']
-        unique_together = ['first_name', 'last_name']
+	class Meta:
+		ordering = ['user']
+		unique_together = ['first_name', 'last_name']
 
-    def __unicode__(self):
-        return "%s %s" % (self.first_name, self.last_name)
+	def __unicode__(self):
+		return "%s %s" % (self.first_name, self.last_name)
 
-    def save(self):
-        if self.user: #denormalization
-            self.first_name = self.user.first_name
-            self.last_name = self.user.last_name
-        super(Staffer, self).save()
+	def save(self):
+		if self.user: #denormalization
+			self.first_name = self.user.first_name
+			self.last_name = self.user.last_name
+		super(Staffer, self).save()
 
-    @models.permalink
-    def get_absolute_url(self):
-        return('staffer_detailed', (), {'slug': self.slug})
+	@models.permalink
+	def get_absolute_url(self):
+		return('staffer_detailed', (), {'slug': self.slug})
 gettag.register(Staffer)
 
 class ContentByline(models.Model):
-    """
-    Abstract base class that handles associating staffers with other types
-    of content.
-    """
-    staffer = models.ForeignKey(Staffer)
-    position = models.CharField(max_length=100)
-    order = models.PositiveSmallIntegerField()
+	"""
+	Abstract base class that handles associating staffers with other types
+	of content.
+	"""
+	staffer = models.ForeignKey(Staffer)
+	position = models.ForeignKey(StaffPosition)
+	order = models.PositiveSmallIntegerField()
 
-    class Meta:
-        abstract = True
+	class Meta:
+		abstract = True
 
-    def __unicode__(self):
-        return "%s - %s" % (self.staffer, self.position)
-
-    def save(self):
-        # denormaliaze position, since a staffer's position may change year-to-year
-        # but the position field should only reflect their position at time of
-        # publication
-        self.position = self.staffer.position
-        super(ContentByline, self).save()
+	def __unicode__(self):
+		return "%s - %s" % (self.staffer, self.position)
