c2e5dff78054b8e58becd5eddc0a6dffa410a129
Max Cutler <max@maxcutler.com>
2010-01-24 02:03:53 +0000

    Cache enabled header rules.

--- courant/core/headers/middleware.py
+++ courant/core/headers/middleware.py
@@ -1,16 +1,21 @@
 from courant.core.headers.models import HeaderRule
 from django.db.models import Q
 import re
+from courant.core.caching import cache
 
 class HttpHeadersMiddleware(object):
 
     def process_request(self, request):
         HEADER_VARS = {}
         extensions_done = False
-        rules = HeaderRule.objects.all().filter(Q(enabled=True) | Q(admin_override=True))
-        
+
+        rules = cache.get('enabled_header_rules')
+        if not rules:
+            rules = HeaderRule.objects.all().filter(Q(enabled=True) | Q(admin_override=True)).select_related()
+            cache.set('enabled_header_rules', rules)
+
         for rule in rules:
-        
+
             """
             This if statement only looks complex because it's multiline
             Basically, if the user is an admin and the admin override is enabled then it works
@@ -23,16 +28,16 @@ class HttpHeadersMiddleware(object):
             re.match(rule.domain, request.META.get('HTTP_HOST')) and \
             re.search(rule.regex, request.META.get("HTTP_" + rule.header.header.replace('-', '_').upper())) \
             ):
-                
+
                 #Only set extensions for first rule that matches
                 if not extensions_done:
                     #Add extensions to request.extension, stripping any leading dots
                     request.extension = [ext.lstrip('.') for ext in rule.extension_list.split(' ')]
                     extensions_done = True
-                        
+
                 #Set context variable if it exists
                 if rule.context_var:
                     HEADER_VARS[rule.context_var] = True
-                
+
         #Attach any context vars to the request object
         request.HEADER_VARS = HEADER_VARS
